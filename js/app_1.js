be679e34bb227ad603829ffbb7d82f5bd00774e9
amcsandborn <amcsandborn@gmail.com>
2015-03-31 20:30:55 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -10,3 +10,18 @@ var map = L.mapbox.map('map', mapId);
 
 //Set the view of the map to the whole US
 map.setView([39, -96], 4);
+
+
+var dataFileToAdd = 'data/avery_test.geojson';
+
+var featureLayer = L.mapbox.featureLayer().loadURL(dataFileToAdd).addTo(map)
+
+featureLayer.on('ready',function(){
+  this.setStyle({
+    'color':'#ec008c',
+    'fillColor':'#ec008c'
+    'weight':4,
+    'opacity':.7
+  });
+  map.fitBounds(featureLayer.getBounds());
+});
